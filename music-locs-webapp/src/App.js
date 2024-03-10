c1573b25e598b33028e27cadb1a4ae198752decd
AlbertDaYoungYT <kcobain465@gmail.com>
2024-03-10 21:46:54 +0100

    Test

--- music-locs-webapp/src/App.js
+++ music-locs-webapp/src/App.js
@@ -12,11 +12,12 @@ const darkTheme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className="flex flex-col justify-center items-start pr-16 bg-neutral-900 max-md:pr-5">
-        <div className="shrink-0 w-24 bg-stone-900 h-[1024px] max-md:hidden" />
+      <div className="flex flex-col justify-center items-start pr-16 bg-[#11140f]">
+        <div className="shrink-0 w-24 bg-[#191d17] h-[100vh] max-md:hidden" />
       </div>
     </ThemeProvider>
   );
 }
+
 
 export default App;
