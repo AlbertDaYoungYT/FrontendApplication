c1206af44fc1561b31bfa27d342adaa4c31f9aee
AlbertDaYoungYT <kcobain465@gmail.com>
2024-03-10 18:50:19 +0100

    First build

--- music-locs-webapp/src/App.js
+++ music-locs-webapp/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Tested successfully
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-start pr-16 bg-neutral-900 max-md:pr-5">
+      <div className="shrink-0 w-24 bg-stone-900 h-[1024px] max-md:hidden" />
     </div>
   );
 }
