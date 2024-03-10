bc9970e399ffa7b2ebbd854d96f029aef361d74e
AlbertDaYoungYT <kcobain465@gmail.com>
2024-03-10 19:58:55 +0100

    App Change

--- music-locs-webapp/src/App.js
+++ music-locs-webapp/src/App.js
@@ -1,10 +1,21 @@
 import * as React from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-start pr-16 bg-neutral-900 max-md:pr-5">
-      <div className="shrink-0 w-24 bg-stone-900 h-[1024px] max-md:hidden" />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="flex flex-col justify-center items-start pr-16 bg-neutral-900 max-md:pr-5">
+        <div className="shrink-0 w-24 bg-stone-900 h-[1024px] max-md:hidden" />
+      </div>
+    </ThemeProvider>
   );
 }
 
